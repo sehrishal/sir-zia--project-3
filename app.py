e784a9015328ec6fbd038ee44128abf542de3798
sehrishal <wwwalikhan2428@gmail.com>
2025-04-26 11:49:56 -0700

    Add files via upload

---
+++ app.py
@@ -0,0 +1,53 @@
+import streamlit as st
+
+def check_password(password):
+    score = 0
+    tips = []
+
+    if len(password) >= 8:
+        score += 1
+    else:
+        tips.append("❤️use at least 8 characters.")
+
+    if any(c.isupper() for c in password):
+        score += 1
+    else:
+        tips.append("💛 Include upper letter.")
+
+    if any(c.islower() for c in password):
+        score += 1
+    else:
+        tips.append("🩷 Include lower letter.")
+
+    if any(c.isdigit() for c in password):
+        score += 1
+    else:
+        tips.append("💚 Add a number(0-9).")
+    
+    if any(c in "!@#$%^&*" for c in password):
+         score += 1
+    else: 
+        tips.append("💜 use a special character (!@#$%^&*)")
+    return score, tips
+
+def main():
+    st.title("🔐 Password Strength Meter")
+    password = st.text_input("Enter Password 🔑", type="password")
+
+    if password:
+        score, tips = check_password(password)
+    
+        if score == 5:
+            st.success("✅ Strong Password! secure & safe.")
+
+        elif score == 3 or score == 4:
+            st.warning("⚠️ Moderate password! Improve it.")
+    
+        else:
+            st.error("❌ Weak Password! Follow these steps:")
+    
+        for tip in tips:
+             st.write(tip)
+    
+       
+main()
